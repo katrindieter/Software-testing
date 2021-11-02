07deba852e39c1f34313252d340602edce2819c8
Katrin Dieter <katrin.dieter@tuni.fi>
2021-11-02 19:10:16 +0200

    environment working

--- add.test.js
+++ add.test.js
@@ -1,7 +1,9 @@
+import add from './src/add'
 
-const sum = require('./src/add');
+
+// const add = require('./src/add.js');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
 });
 
